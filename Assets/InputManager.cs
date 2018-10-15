b31dd40efd6805253fb9bc0b2faa43679cdd14d5
Neil Hansen <neilmickneilh12@hotmail.com>
2018-10-14 21:37:22 -0400

    added input for movement

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class InputManager : MonoBehaviour {
 
     private AudioManager audioManager;
+    public float offset;
+    private GameObject player;
 
 	// Use this for initialization
 	void Start () {
         audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();
-         }
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -22,6 +25,23 @@ public class InputManager : MonoBehaviour {
         {
             Debug.Log("Missed Beat");
         }
-		
-	}
+
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            player.transform.position += new Vector3(-offset, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            player.transform.position += new Vector3(offset, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            player.transform.position += new Vector3(0, 0, -offset);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            player.transform.position += new Vector3(0, 0, offset);
+        }
+
+    }
 }
